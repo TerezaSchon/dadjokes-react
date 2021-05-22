a479f6855cfc8c7be5c6f2a3426833a5e9e0dd86
TerezaSchon <tereza.vlachova87@gmail.com>
2021-05-22 17:16:39 +0200

    mezifáze, kdy se mění text vtipu, ale avatar zmizel, jména taky, čísla nedělají, co mají a není to moc vtipné

---
+++ src/Joke/index.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import '../style.css';
+
+export const Joke = (props) => {
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
+
+  const handleClickLike = () => {
+    setLike(like + 1);
+  };
+
+  const handleClickDislike = () => {
+    setDislike(dislike + 1);
+  };
+
+  return (
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src={props.avatar} />
+
+            <p className="user-name">{props.name}</p>
+          </div>
+
+          <p className="joke__text">{props.text}</p>
+        </div>
+        <div className="joke__likes">
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={handleClickLike}
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {props.likes}
+          </span>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={handleClickDislike}
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {props.dislikes}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+render;
